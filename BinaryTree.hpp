44bf4a50e0d8eeaec1fcb1afc94533625db8ed48
Itai Lashover <itai.lash@gmail.com>
2021-05-25 21:44:19 +0300

    finish part A

--- BinaryTree.hpp
+++ BinaryTree.hpp
@@ -1,9 +1,9 @@
 #pragma once
+
 #include <iostream>
-#include <ostream>
+#include <string>
 #include <vector>
-#include <iomanip>
-#include <cstring>
+
 
 namespace ariel {
 
@@ -28,68 +28,95 @@ namespace ariel {
     public:
         BinaryTree() : root(nullptr) {}
         ~BinaryTree() { delete root; }
-        BinaryTree &add_root(T value);
-        BinaryTree &add_left(T existing_value, T new_value);
-        BinaryTree &add_right(T existing_value, T new_value);
-        friend std::ostream &operator<<(std::ostream &os, const BinaryTree<T> &tree);
+        Node *find(Node *n, T val);
+        BinaryTree &add_root(T val);
+        BinaryTree &add_left(T val, T new_val);
+        BinaryTree &add_right(T val, T new_val);
 
         class Iterator{
-        private:
-            Node *node;
+            private:
+                Node *pointer_to_current_node;
+                void (*func)();
+                std::vector<Node*> vec;
 
-        public:
-            Iterator(Node *ptr = nullptr) : node(ptr) {}
-            T &operator*() const {return node->value;}
-            Iterator &operator++() {return *this;}  //prefox
-            Iterator operator++(int) {return *this;} //postfix
-            bool operator==(const Iterator &it) const {return false;}
-            bool operator!=(const Iterator &it) const {return false;}
-            Iterator &operator=(const Iterator &iterator){return iterator;}
-            T *operator->()const{T ans;
-            return ans;}
+            public:
+                Iterator(Node *ptr = nullptr, void (*selected_order)() = nullptr) : 
+                pointer_to_current_node(ptr), func(selected_order){}
+                static void pre_order() {};
+                static void in_order() {};
+                static void post_order() {};
+                T &operator*() const {return pointer_to_current_node->value;}
+                T *operator->()const{return &(pointer_to_current_node->value);}
+                Iterator &operator++(){return *this;}
+                Iterator operator++(int){return *this;}
+                bool operator==(const Iterator &it) const {return false;}
+                bool operator!=(const Iterator &it) const {return false;}
         };
 
        
-        Iterator begin_inorder() {return Iterator();} 
+        Iterator begin_inorder() {return Iterator(root, &Iterator::in_order);} 
         Iterator end_inorder() {return Iterator();} 
-        Iterator begin_preorder() {return Iterator();}
+        Iterator begin_preorder() {return Iterator(root, &Iterator::pre_order);}
         Iterator end_preorder() {return Iterator();}
-        Iterator begin_postorder() {return Iterator();}
+        Iterator begin_postorder() {return Iterator(root, &Iterator::post_order);}
         Iterator end_postorder() {return Iterator();}
+        Iterator begin() { return begin_inorder(); };
+        Iterator end() { return Iterator(); };
 
+        friend std::ostream &operator<<(std::ostream &os, const BinaryTree<T> &tree){
+            return os << "my binary tree" << std::endl;
+        }
     };
 
-    ////////////////////////////////
-    //////  Implementations:  //////
-    ////////////////////////////////
-
-    /*
-     * BinaryTree:
-     */
+    template<typename T>
+    typename BinaryTree<T>::Node *BinaryTree<T>::find(BinaryTree::Node *n, T val) {
+        if (n == nullptr || n->value == val) {
+            return n;
+        }
+        Node *find_left = find(n->left, val);
+        Node *find_right = find(n->right, val);
+        if (find_left == nullptr) {
+            return find_right;
+        } 
+        else {
+            return find_left;
+        }
+    }  
 
     template<typename T>
-    BinaryTree<T> &BinaryTree<T>::add_root(T value) {
-        if (root == nullptr) 
-        {
-            root = new Node{value};
+    BinaryTree<T> &BinaryTree<T>::add_root(T val) {
+        if (root == nullptr) { root = new Node{val}; } 
+        else { root->value = val; }
+        return *this;
+    }
+
+    template<typename T>
+    BinaryTree<T> &BinaryTree<T>::add_left(T val, T new_val) {
+        Node *n = find(root, val);
+        if (n == nullptr) {
+            throw std::runtime_error("Error: val is not existing in the tree.\n");
+        }
+        if (n->left == nullptr) {
+            n->left = new Node{new_val};
         } 
-        else 
-        {
-            root->value = value;
+        else {
+            n->left->value = new_val;
         }
         return *this;
     }
 
     template<typename T>
-    BinaryTree<T> &BinaryTree<T>::add_left(T existing_value, T new_value) {
+    BinaryTree<T> &BinaryTree<T>::add_right(T val, T new_val) {
+        Node *n = find(root, val);
+        if (n == nullptr) {
+            throw std::runtime_error("Error: existing_value is no exist in the tree.\n");
+        }
+        if (n->right == nullptr) {
+            n->right = new Node{new_val};
+        } 
+        else {
+            n->right->value = new_val;
+        }
         return *this;
     }
-
-    template<typename T>
-    BinaryTree<T> &BinaryTree<T>::add_right(T existing_value, T new_value) {
-      
-        return *this;
-    }
-
-
 } 
---
+++ Test.cpp
@@ -0,0 +1,233 @@
+#include "BinaryTree.hpp"
+#include "doctest.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace ariel;
+using namespace std;
+
+BinaryTree<int> bt;
+void init_tree();
+/*********************************************************************/
+/*                         Int Binary Tree                           */
+/*********************************************************************/
+TEST_CASE ("add root & left & right") {
+    BinaryTree<int> ibt;
+    CHECK_NOTHROW(ibt.add_root(1).add_left(1, 2).add_right(1,3));
+    CHECK_NOTHROW(ibt.add_root(11).add_root(12).add_root(13).add_root(14));
+    CHECK_EQ(*(ibt.begin()), 14);
+    CHECK_NE(*(ibt.begin()), 1);
+    CHECK_THROWS(ibt.add_left('a', 'b'));
+    CHECK_THROWS(ibt.add_left(2, 4).add_right(4, 'c').add_left('c', 'd'));
+}
+
+TEST_CASE ("inorder traversal") {
+    init_tree();
+    string expected = "8 4 9 12 2 5 1 6 3 13 10 7 11 ";
+    string actual = "";
+
+    for (auto it = bt.begin_inorder(); it != bt.end_inorder(); it ++) {
+            actual += to_string(*it) + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+
+    string actual_default = "";
+    for (auto it = bt.begin(); it != bt.end(); it ++) {
+            actual += to_string(*it) + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+}
+
+TEST_CASE ("preorder traversal") {
+    init_tree();
+    string expected = "1 2 4 8 9 12 5 3 6 7 10 15 11 ";
+    string actual = "";
+
+    for (auto it = bt.begin_preorder(); it != bt.end_preorder(); it ++) {
+            actual += to_string(*it) + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+}
+
+TEST_CASE ("postorder traversal") {
+    init_tree();
+    string expected = "8 12 9 4 5 2 6 13 10 11 7 3 1 ";
+    string actual = "";
+
+    for (auto it = bt.begin_postorder(); it != bt.end_postorder(); it ++) {
+            actual += to_string(*it) + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+}
+
+TEST_CASE ("Iterator") {
+    init_tree();
+    auto in_it = bt.begin_inorder();
+    auto pr_it = bt.begin_preorder();
+    auto po_it = bt.begin_postorder();
+
+    CHECK(*in_it == 8);
+    CHECK(*pr_it == 1);
+    CHECK(*po_it== 8);
+
+    CHECK(*(in_it++) == 8);
+    CHECK(*(in_it) == 4);
+    CHECK(*(++in_it) == 9);
+
+    CHECK(*(pr_it++) == 1);
+    CHECK(*(pr_it) == 2);
+    CHECK(*(++pr_it) == 4);
+
+    CHECK(*(po_it++) == 8);
+    CHECK(*(po_it) == 12);
+    CHECK(*(++po_it) == 9);
+}
+    
+/**              1
+ *              / \
+ *             2   3
+ *            / \ / \
+ *           4  5 6  7
+ *          / \     / \
+ *         8   9   10 11
+ *              \  /
+ *             12 13
+ **/
+
+void init_tree(){
+     bt.add_root(1).add_left(1,2).add_right(1,3)
+     .add_left(2,4).add_right(2,5).add_left(3,6).add_right(3,7)
+     .add_left(4,8).add_right(4,9).add_left(7,10).add_right(7,11)
+     .add_right(9,12).add_left(10,13);
+}
+
+
+class Person{
+public:
+    const string name;
+    int age;
+    Person(string n, int a) : name(n), age(a){};
+    Person& operator=(const Person& other){
+        return *this;
+    }
+    bool operator==(const Person &p) const{return name == p.name && age == p.age;};
+    bool operator!=(const Person &p) const{return !(*this==p);};
+    // friend std::ostream &operator<<(std::ostream &os, const Person &p){
+    //     return os << p.name << std::endl;
+    // }
+};
+
+BinaryTree<Person> person_bt;
+void init_person_tree();
+/*********************************************************************/
+/*                       Person Binary Tree                          */
+/*********************************************************************/
+TEST_CASE ("add root & left & right") {
+    BinaryTree<Person> pbt;
+    Person avi{"Avi", 30};
+    Person batia{"Batia", 22};
+    Person chris{"Chris", 12};
+    Person david{"David", 43};
+    Person efrat{"Efrat", 37};
+    Person fatma{"Fatma", 62};
+    Person gadi{"Gadi", 50};
+    Person haim{"Haim", 81};
+    Person itai{"Itai", 25};
+
+    CHECK_NOTHROW(pbt.add_root(avi).add_left(avi, batia).add_right(avi,chris));
+    CHECK_NOTHROW(pbt.add_root(gadi).add_root(haim).add_root(itai));
+    CHECK_EQ(*(pbt.begin()), itai);
+    CHECK_NE(*(pbt.begin()), avi);
+    CHECK_THROWS(pbt.add_left(david, gadi));
+}
+
+TEST_CASE ("inorder traversal") {
+    init_person_tree();
+    string expected = "David Haim Gadi Itai Batia Avi Efrat Chris Fatma ";
+    string actual = "";
+
+    for (auto it = person_bt.begin_inorder(); it != person_bt.end_inorder(); it ++) {
+            actual += (*it).name + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+
+    string actual_default = "";
+    for (auto it = person_bt.begin(); it != person_bt.end(); it ++) {
+            actual += (*it).name + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+}
+
+TEST_CASE ("preorder traversal") {
+    init_person_tree();
+    string expected = "Avi Batia David Gadi Haim Itai Chris Efrat Fatma ";
+    string actual = "";
+
+    for (auto it = person_bt.begin_preorder(); it != person_bt.end_preorder(); it ++) {
+            actual += (*it).name + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+}
+
+TEST_CASE ("postorder traversal") {
+    init_person_tree();
+    string expected = "Haim Itai Gadi David Batia Efrat Fatma Chris Avi ";
+    string actual = "";
+
+    for (auto it = person_bt.begin_postorder(); it != person_bt.end_postorder(); it ++) {
+            actual += (*it).name + " ";
+    }
+    
+    CHECK_EQ(actual, expected);
+}
+
+TEST_CASE ("Iterator") {
+    init_person_tree();
+    Person david{"David",43};
+    Person gadi{"Gadi", 50};
+    Person haim{"Haim", 81};
+
+    auto in_it = person_bt.begin();
+    CHECK(*in_it == david);
+    CHECK(*(in_it++) == david);
+    CHECK(*(in_it) == haim);
+    CHECK(*(++in_it) == gadi);
+
+}
+    
+/**             avi
+ *              / \
+ *          batia chris
+ *            /    /   \
+ *         david efrat fatma
+ *            \    
+ *           gadi
+ *           /  \
+ *         haim itai
+ **/
+
+void init_person_tree(){
+    Person avi{"Avi", 30};
+    Person batia{"Batia", 22};
+    Person chris{"Chris", 12};
+    Person david{"David", 43};
+    Person efrat{"Efrat", 37};
+    Person fatma{"Fatma", 62};
+    Person gadi{"Gadi", 50};
+    Person haim{"Haim", 81};
+    Person itai{"Itai", 25};
+     person_bt.add_root(avi).add_left(avi,batia).add_right(avi,chris)
+     .add_left(batia,david).add_left(chris, efrat).add_right(chris,fatma)
+     .add_right(david,gadi).add_left(gadi,haim).add_right(gadi,itai);
+}
+
+
+
